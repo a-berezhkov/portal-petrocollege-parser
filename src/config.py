904fa99da2301fa23ca3c089310af60052697d50
Anddrei V. Berreezhkov <dead0343@gmail.com>
2022-11-14 15:39:57 +0300

    init

---
+++ src/config.py
@@ -0,0 +1,9 @@
+user = dict(
+    username=r'login',
+    password='pass'
+)
+urls = dict(
+    basic_url=r"https://portal.petrocollege.ru/_api/",
+    basic_web_url=r"https://portal.petrocollege.ru/_api/web/",
+    schedule_link=r"https://portal.petrocollege.ru/_api/Web/Lists(guid'9c095153-274d-4c73-9b8b-4e3dd6af89e5')/Items"
+)
